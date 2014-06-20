7e785ca1b2c8d609a6b08fe25101b4b5224578aa
daiyaochu <daiyaochu08@aliyun.com>
2014-06-20 23:42:53 +0800

    112

---
+++ WebApplication1/default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using RazorEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string template = "Hello @Model.Name! Welcome to Razor!";
+            string result = Razor.Parse(template, new { Name = "World" });
+            Response.Write(result);
+        }
+    }
+}
